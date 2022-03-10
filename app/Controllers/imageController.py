fd4956903f283862fe276bf3a498253c8eb1409f
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2022-03-10 16:17:32 -0300

    Initial release

---
+++ app/Controllers/imageController.py
@@ -0,0 +1,146 @@
+import numpy as np
+from typing import NamedTuple
+
+class componentsSelection(NamedTuple):
+    StartPoint: tuple
+    EndPoint: tuple
+    label: str = ""
+
+class imageProperties():
+    imageLocation = ""
+    selectedAreas = []
+    dragging = False
+    defaultAreaPickerSize = 15
+    
+
+    def __init__(self, imageLocation):
+        self.imageLocation = imageLocation
+
+    def addArea(self, start_point, end_point):
+        self.selectedAreas.append(componentsSelection(start_point, end_point))
+
+    def returnAreaSelected(self):
+        return self.selectedAreas
+
+    def drawImage(self, window, graph):
+        start_point = end_point = prior_rect = None
+        event, values = window.read()
+        while not event.endswith('+UP'):
+            
+            x, y = values["-GRAPH-"]
+            event, values = window.read()
+            
+            if not self.dragging:
+                start_point = (x, y)
+                self.dragging = True
+            else:
+                end_point = (x, y)
+
+            if prior_rect:
+                graph.delete_figure(prior_rect)
+            if None not in (start_point, end_point):
+                prior_rect = graph.draw_rectangle(start_point, end_point, line_color='red')
+
+        if event.endswith('+UP'):  # The drawing has ended because mouse up
+            if start_point!=None and end_point!=None:
+                graph.draw_image(self.imageLocation, location=(0,270))
+                #Inversão de ordem do ponto inicial para que a referência inicial seja sempre o canto esquerdo superior
+                if start_point[0]>end_point[0]:
+                    hold = end_point[0]
+                    end_point = (start_point[0], end_point[1])
+                    start_point = (hold, start_point[1])
+                if start_point[1]<end_point[1]:
+                    hold = end_point[1]
+                    end_point = (end_point[0], start_point[1])
+                    start_point = (start_point[0], hold)
+
+                self.addArea(start_point, end_point)
+
+                graph.delete_figure(prior_rect)
+                info = window["info"]
+                info.update(value=f"grabbed rectangle from {start_point} to {end_point}")
+                start_point, end_point = None, None  # enable grabbing a new rect
+
+                for rectangles in self.returnAreaSelected():
+                    graph.draw_rectangle(rectangles.StartPoint, rectangles.EndPoint, line_color='red')
+            self.dragging = False
+
+    def moveRectangle(self, window, graph, idx):
+        points = self.returnAreaSelected()
+        cursor = "fleur"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+        prior_rect = None
+
+        rectSizeX = points[idx].EndPoint[0]-points[idx].StartPoint[0]
+        rectSizeY = points[idx].EndPoint[1]-points[idx].StartPoint[1]  
+
+        event, values = window.read()
+
+        if event == "-GRAPH-":  # if there's a "Graph" event, then it's a mouse 
+            while not event.endswith('+UP'):
+                event, values = window.read(timeout=0)
+
+                newInitialPos = values["-GRAPH-"]
+                newEndPos = tuple(np.add(points[idx].StartPoint, (rectSizeX, rectSizeY)))
+                
+                points[idx] = points[idx]._replace(StartPoint = newInitialPos)
+                points[idx] = points[idx]._replace(EndPoint = newEndPos)
+                
+                if prior_rect:
+                    graph.erase()
+                    graph.draw_image(self.imageLocation, location=(0,270)) if self.imageLocation else None
+
+                for drawrectangles in points:
+                    prior_rect = graph.draw_rectangle(drawrectangles.StartPoint, drawrectangles.EndPoint, line_color='red')
+        cursor = "arrow"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+    def eraseRectangle(self, window, graph, idx):
+        
+        cursor = "fleur"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+        prior_rect = None
+
+        event, values = window.read()
+
+        if event.endswith('CLICK+'):  # if there's a "Graph" event, then it's a mouse 
+            self.deleteRectangle(idx)
+            points = self.returnAreaSelected()
+            
+            graph.erase()
+            graph.draw_image(self.imageLocation, location=(0,270)) if self.imageLocation else None
+
+            for drawrectangles in points:
+                prior_rect = graph.draw_rectangle(drawrectangles.StartPoint, drawrectangles.EndPoint, line_color='red')
+        cursor = "arrow"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+
+    def resizeRectangle(self, window, idx, graph):
+        points = self.returnAreaSelected()
+        cursor = "bottom_right_corner"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+        prior_rect = None
+
+        event, values = window.read()
+
+        if event == "-GRAPH-":  # if there's a "Graph" event, then it's a mouse 
+            while not event.endswith('+UP'):
+                x, y = values["-GRAPH-"]
+                event, values = window.read()
+                if (points[idx].StartPoint[0]+self.defaultAreaPickerSize < x) and (points[idx].StartPoint[1]-self.defaultAreaPickerSize > y):
+                    points[idx] = points[idx]._replace(EndPoint = (x,y))
+
+                if prior_rect:
+                    graph.erase()
+                    graph.draw_image(self.imageLocation, location=(0,270)) if self.imageLocation else None
+                for drawrectangles in points:
+                    prior_rect = graph.draw_rectangle(drawrectangles.StartPoint, drawrectangles.EndPoint, line_color='red')
+        cursor = "arrow"
+        window.find_element("-GRAPH-").set_cursor(cursor)
+
+    def deleteRectangle(self, idx):
+        self.selectedAreas.pop(idx)
---
+++ main.py
@@ -0,0 +1,87 @@
+import PySimpleGUI as sg
+import numpy as np
+
+"""
+    Demo - Drag a rectangle to draw it
+    This demo shows how to use a Graph Element to (optionally) display an image and then use the
+    mouse to "drag a rectangle".  This is sometimes called a rubber band and is an operation you
+    see in things like editors
+"""
+
+from app.Controllers.imageController import imageProperties as imgC
+
+image_file = r'C:\Users\gely\Desktop\Desenvolvimento\Python\SimplePyImg\savedResults\Default_1.png'  # image is optional
+
+imageProps = imgC(image_file)
+
+layout = [
+    [
+        sg.Graph(
+        canvas_size=(480, 270),
+        graph_bottom_left=(0, 0),
+        graph_top_right=(480, 270),
+        key="-GRAPH-",
+        change_submits=True,  # mouse click events
+        drag_submits=True,
+        enable_events=True,
+        background_color='lightblue'
+        ), 
+    ],
+    [
+        sg.Text(
+            key='info',
+            size=(60, 1)
+        )
+    ]
+]
+
+window = sg.Window("draw rect on image", layout, finalize=True)
+
+# get the graph element for ease of use later
+graph = window["-GRAPH-"]  # type: sg.Graph
+
+graph.bind('<Enter>', '+MOUSE OVER+')
+graph.bind('<Leave>', '+MOUSE AWAY+')
+graph.bind('<Motion>', '+MOVED+')
+graph.bind('<Button-2>', '+MIDDLE CLICK+')
+
+graph.draw_image(image_file, location=(0,270)) if image_file else None
+dragging = False
+start_point = end_point = prior_rect = None
+points = []
+
+cursor = "arrow"
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break  # exit
+
+    if event == "-GRAPH-":  # if there's a "Graph" event, then it's a mouse
+        imageProps.drawImage(window, graph)
+
+
+    if event.endswith('+MOVED+'):
+        if(imageProps.returnAreaSelected()):
+            points = imageProps.returnAreaSelected()
+            x, y = values["-GRAPH-"]
+            for idx, rectangles in enumerate(points):
+                
+                lowerLimit = tuple(np.subtract(rectangles.StartPoint, (imageProps.defaultAreaPickerSize,imageProps.defaultAreaPickerSize)))
+                upperLimit = tuple(np.add(rectangles.StartPoint, (imageProps.defaultAreaPickerSize,imageProps.defaultAreaPickerSize)))
+
+                lowerLimitResize = tuple(np.subtract(rectangles.EndPoint, (imageProps.defaultAreaPickerSize,imageProps.defaultAreaPickerSize)))
+                upperLimitResize = tuple(np.add(rectangles.EndPoint, (imageProps.defaultAreaPickerSize,imageProps.defaultAreaPickerSize)))         
+                
+                while x <= upperLimit[0] and x >= lowerLimit[0] and y <= upperLimit[1] and y >= lowerLimit[1]:
+                    imageProps.moveRectangle(window, graph, idx)
+                    imageProps.eraseRectangle(window, graph, idx)
+                    break
+                            
+                while x <= upperLimitResize[0] and x >= lowerLimitResize[0] and y <= upperLimitResize[1] and y >= lowerLimitResize[1]:
+                    imageProps.resizeRectangle(window, idx, graph)
+                    break
+    else:
+        pass
+        print("unhandled event", event, values)
